b8a7c7a894dc3506e8eb8aa8d746ec2d49d8d259
LeeJeongSeok <ljs0429777@gmail.com>
2021-10-17 22:00:35 +0900

    Amazon S3 환경 설정

--- src/main/java/com/team/hash/hashproject/HashProjectApplication.java
+++ src/main/java/com/team/hash/hashproject/HashProjectApplication.java
@@ -2,12 +2,17 @@ package com.team.hash.hashproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class HashProjectApplication {
 
+    public static final String APPLICATION_LOCATIONS = "spring.config.location=" + "classpath:application.properties";
+
     public static void main(String[] args) {
-        SpringApplication.run(HashProjectApplication.class, args);
+        new SpringApplicationBuilder(HashProjectApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
     }
 
 }
